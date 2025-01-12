a0c6e67b36a82bd8c967f6fe8e0605a29eafb80d
Md.Mustakim Billah <mdmustakimkhan279@gmail.com>
2025-01-12 13:41:16 +0600

    Error message show in ui

--- js/custom.js
+++ js/custom.js
@@ -6,23 +6,32 @@ const editBtnElm = document.querySelector('.edit-btn');
 const deleteBtnElm = document.querySelector('.delete-btn');
 const CompleteBtnElm = document.querySelector('.Complete-btn');
 const undoBtnElm = document.querySelector('.undo-btn');
+const messageElm = document.querySelector('.message');
+
 
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+//messageElm.textContent = tasks.length || 'no tasks available';
 
 const addEventListeners = () => {
     addBtnElm.addEventListener('click',addTask);
+    inputElm.addEventListener('input', inputChanges)
     //inputElm.addEventListener('keypress',addInput);
    // searchElm.addEventListener('input',searchTask);
+ }
+ const inputChanges = () => {
+    messageElm.textContent = '';
  }
 
 const addTask = () => {
     const task = inputElm.value
+    const id = tasks.length + 1;
     if (task === ''){
-        alert('Please enter a task')
+        messageElm.textContent = 'Please Enter a task';
+        //alert('Please enter a task')
         return;
     }
     //console.log(task);
-    tasks.push({task,Completed: false});
+    tasks.push({ id,task,Completed: false});
     localStorage.setItem('tasks',JSON.stringify(tasks));
     renderTasks();
     inputElm.value = '' ;
@@ -45,6 +54,7 @@ const renderTasks = () => {
 
         taskListElm.appendChild(taskElm);
     })
+    messageElm.textContent = tasks.length === 0 ? 'no tasks available' : "" ;
  }
  renderTasks();
  addEventListeners();
